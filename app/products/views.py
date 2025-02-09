5f7ae4e722220d45fe1db6f08c1836c3ccac2a2a
GlebNotss <gatsenko.g@mail.ru>
2025-02-10 01:02:47 +0300

    fix

--- app/products/views.py
+++ app/products/views.py
@@ -127,7 +127,7 @@ class MainPageView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] =  Category.objects.prefetch_related('products').all() 
-        context['suppliers'] = Supplier.objects.prefetch_related('category').all() 
+        context['suppliers'] = Supplier.objects.prefetch_related('product').all()
         return context
 
 class BaseView(ListView):
