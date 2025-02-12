4c4d1e77412a32e153d1230f2f47e45a331854e9
GlebNotss <gatsenko.g@mail.ru>
2025-02-12 19:11:56 +0300

    Страница контактов

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -90,7 +90,6 @@ USE_TZ = True
 # Статика и медиа
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
